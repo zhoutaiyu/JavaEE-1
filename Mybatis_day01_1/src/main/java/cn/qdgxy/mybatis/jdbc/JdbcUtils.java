2b05cf2a0395801a0ca9a4abaab14c7b0296c8a2
lixin <1191863273@qq.com>
2016-10-13 21:14:20 +0800

    create

---
+++ Mybatis_day01_1/src/main/java/cn/qdgxy/mybatis/jdbc/JdbcUtils.java
@@ -0,0 +1,113 @@
+package cn.qdgxy.mybatis.jdbc;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.*;
+import java.util.Properties;
+
+/**
+ * JDBC小工具
+ *
+ * @author 李欣
+ * @version 2016-10-13 20:29
+ */
+public class JdbcUtils {
+
+    private static Properties props = null;
+
+    // 只在JdbcUtils类被加载时执行一次
+    static {
+        // 给props进行初始化，即加载dbconfig.properties到props对象中
+        try {
+            InputStream in = JdbcUtils.class.getClassLoader()
+                    .getResourceAsStream("dbconfig.properties");
+            props = new Properties();
+            props.load(in);
+            in.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        // 加载驱动类
+        try {
+            Class.forName(props.getProperty("driverClassName"));
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * 得到连接
+     *
+     * @return Connection
+     * @throws SQLException SQL异常
+     */
+    public static Connection getConnection() throws SQLException {
+        // 1. 加载配置文件
+        // 2. 加载驱动类
+        // 3. 调用DriverManger.getConnection()
+        return DriverManager.getConnection(props.getProperty("url"),
+                props.getProperty("username"), props.getProperty("password"));
+    }
+
+    /**
+     * 关闭连接
+     *
+     * @param conn Connection
+     */
+    public static void closeConnection(Connection conn) {
+        try {
+            if (conn != null) {
+                conn.close();
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * 关闭连接
+     *
+     * @param conn Connection
+     * @param stmt Statement
+     */
+    public static void closeConnection(Connection conn, Statement stmt) {
+        try {
+            if (stmt != null) {
+                stmt.close();
+            }
+
+            if (conn != null) {
+                conn.close();
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * 关闭连接
+     *
+     * @param conn Connection
+     * @param stmt Statement
+     * @param rs   ResultSet
+     */
+    public static void closeConnection(Connection conn, Statement stmt, ResultSet rs) {
+        try {
+            if (rs != null) {
+                rs.close();
+            }
+
+            if (stmt != null) {
+                stmt.close();
+            }
+
+            if (conn != null) {
+                conn.close();
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}

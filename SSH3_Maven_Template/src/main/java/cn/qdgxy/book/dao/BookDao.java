5db5f53f6e859af8209b3e0410a609b0e6536b15
lixin <1191863273@qq.com>
2016-10-21 09:10:59 +0800

    finish SSH3_Maven_Template

--- SSH3_Maven_Template/src/main/java/cn/qdgxy/book/dao/BookDao.java
+++ SSH3_Maven_Template/src/main/java/cn/qdgxy/book/dao/BookDao.java
@@ -1,84 +1,79 @@
 package cn.qdgxy.book.dao;
 
 import cn.qdgxy.book.vo.Book;
-import org.springframework.orm.hibernate5.HibernateTemplate;
+import org.springframework.orm.hibernate5.support.HibernateDaoSupport;
 import org.springframework.stereotype.Repository;
 
-import javax.annotation.Resource;
-
 @Repository
-public class BookDao {
+public class BookDao extends HibernateDaoSupport {
 
-    @Resource
-	private HibernateTemplate hibernateTemplate;
+    /**
+     * 保存图书
+     *
+     * @param book
+     */
+    public void save(Book book) {
+        System.out.println("Dao 添加图书。。。。。。。。");
+        this.getHibernateTemplate().save(book);
+    }
 
-	/**
-	 * 保存图书
-	 * 
-	 * @param book
-	 */
-	public void save(Book book) {
-		System.out.println("Dao 添加图书。。。。。。。。");
-		hibernateTemplate.save(book);
-	}
-
-	/**
-	 * 修改图书
-	 * 
-	 * @param book
-	 */
-	/*
-	 * public void update(Book book) { this.getHibernateTemplate().update(book);
+    /**
+     * 修改图书
+     *
+     * @param book
+     */
+    /*
+     * public void update(Book book) { this.getHibernateTemplate().update(book);
 	 * }
 	 *//**
-	 * 删除图书
-	 * 
-	 * @param book
-	 */
-	/*
-	 * public void delete(Book book) { this.getHibernateTemplate().delete(book);
+     * 删除图书
+     *
+     * @param book
+     */
+    /*
+     * public void delete(Book book) { this.getHibernateTemplate().delete(book);
 	 * }
 	 *//**
-	 * 按ID查询
-	 * 
-	 * @param id
-	 * @return
-	 */
+     * 按ID查询
+     *
+     * @param id
+     * @return
+     */
 	/*
 	 * public Book findById(Integer id) { return
 	 * this.getHibernateTemplate().get(Book.class, id); }
 	 *//**
-	 * 查找所有
-	 * 
-	 * @return
-	 */
+     * 查找所有
+     *
+     * @return
+     */
 	/*
 	 * @SuppressWarnings("unchecked") public List<Book> findAll() { return
 	 * (List<Book>) this.getHibernateTemplate().find("from Book"); }
 	 *//**
-	 * 按条件查询
-	 * 
-	 * @return
-	 */
+     * 按条件查询
+     *
+     * @return
+     */
 	/*
 	 * @SuppressWarnings("unchecked") public List<Book>
 	 * findByCriteria(DetachedCriteria criteria) { return (List<Book>)
 	 * this.getHibernateTemplate() .findByCriteria(criteria); }
 	 *//**
-	 * 按名称查询
-	 * 
-	 * @return
-	 */
+     * 按名称查询
+     *
+     * @return
+     */
 	/*
 	 * @SuppressWarnings("unchecked") public List<Book> findByName(String name)
 	 * { return (List<Book>) this.getHibernateTemplate().findByNamedQuery(
 	 * "findByName", name); }
 	 *//**
-	 * 按ID查询(延迟)
-	 * 
-	 * @param id
-	 * @return
-	 */
+     * 按ID查询(延迟)
+     *
+     * @param id
+     * @return
+     */
 	/*
 	 * public Book findByIdLazy(Integer id) { return
 	 * this.getHibernateTemplate().load(Book.class, id); }

521a77a7f097767db2dffdb441f7852630787085
lixin <1191863273@qq.com>
2016-10-22 21:06:25 +0800

    finish

---
+++ MybatisTool/src/main/java/cn/qdgxy/mybatis/until/GeneratorSqlmap.java
@@ -0,0 +1,40 @@
+package cn.qdgxy.mybatis.until;
+
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+import java.io.File;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+public class GeneratorSqlmap {
+
+    public void generator() throws Exception {
+
+        List<String> warnings = new ArrayList<>();
+        final boolean overwrite = true;
+        //指定逆向工程配置文件
+        URL url = GeneratorSqlmap.class.getClassLoader().getResource("Mybatis/generatorConfig.xml");
+        File configFile = new File(url.getFile());
+        ConfigurationParser cp = new ConfigurationParser(warnings);
+        Configuration config = cp.parseConfiguration(configFile);
+        DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+        MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config,
+                callback, warnings);
+        myBatisGenerator.generate(null);
+
+    }
+
+    public static void main(String[] args) throws Exception {
+        try {
+            GeneratorSqlmap generatorSqlmap = new GeneratorSqlmap();
+            generatorSqlmap.generator();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}

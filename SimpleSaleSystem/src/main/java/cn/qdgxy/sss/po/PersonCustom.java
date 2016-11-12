9de6b443668df5e3df3337d069a84e768cc21fa1
李欣 <1191863273@qq.com>
2016-11-12 20:37:59 +0800

    update

---
+++ SimpleSaleSystem/src/main/java/cn/qdgxy/sss/po/PersonCustom.java
@@ -0,0 +1,12 @@
+package cn.qdgxy.sss.po;
+
+/**
+ * 自定义用户
+ * create:2016-11-12 20:35
+ *
+ * @author 李欣
+ * @version 1.0
+ */
+public class PersonCustom extends Person {
+
+}
---
+++ SimpleSaleSystem/src/main/java/cn/qdgxy/sss/po/TrxCustom.java
@@ -0,0 +1,12 @@
+package cn.qdgxy.sss.po;
+
+/**
+ * 自定义订单
+ * create:2016-11-12 20:36
+ *
+ * @author 李欣
+ * @version 1.0
+ */
+public class TrxCustom extends Trx {
+
+}

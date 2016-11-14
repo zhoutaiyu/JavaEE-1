a73fd761bda0c8ec89497c7fad44691c060c2b01
李欣 <1191863273@qq.com>
2016-11-14 14:16:13 +0800

    update

---
+++ MybatisTool/src/main/java/cn/qdgxy/sss/po/Order.java
@@ -0,0 +1,65 @@
+package cn.qdgxy.sss.po;
+
+import java.io.Serializable;
+
+public class Order implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    private Integer id;
+    private Integer pid;
+    private Integer uid;
+    private Double buyPrice;
+    private String buyTime;
+
+    public Order(Integer id, Integer pid, Integer uid, Double buyPrice, String buyTime) {
+        this.id = id;
+        this.pid = pid;
+        this.uid = uid;
+        this.buyPrice = buyPrice;
+        this.buyTime = buyTime;
+    }
+
+    public Order() {
+        super();
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getPid() {
+        return pid;
+    }
+
+    public void setPid(Integer pid) {
+        this.pid = pid;
+    }
+
+    public Integer getUid() {
+        return uid;
+    }
+
+    public void setUid(Integer uid) {
+        this.uid = uid;
+    }
+
+    public Double getBuyPrice() {
+        return buyPrice;
+    }
+
+    public void setBuyPrice(Double buyPrice) {
+        this.buyPrice = buyPrice;
+    }
+
+    public String getBuyTime() {
+        return buyTime;
+    }
+
+    public void setBuyTime(String buyTime) {
+        this.buyTime = buyTime == null ? null : buyTime.trim();
+    }
+}
---
+++ MybatisTool/src/main/java/cn/qdgxy/sss/po/Product.java
@@ -0,0 +1,75 @@
+package cn.qdgxy.sss.po;
+
+import java.io.Serializable;
+
+public class Product implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    private Integer id;
+    private Double price;
+    private String title;
+    private String image;
+    private String summary;
+    private byte[] detail;
+
+    public Product(Integer id, Double price, String title, String image, String summary, byte[] detail) {
+        this.id = id;
+        this.price = price;
+        this.title = title;
+        this.image = image;
+        this.summary = summary;
+        this.detail = detail;
+    }
+
+    public Product() {
+        super();
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title == null ? null : title.trim();
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image == null ? null : image.trim();
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public void setSummary(String summary) {
+        this.summary = summary == null ? null : summary.trim();
+    }
+
+    public byte[] getDetail() {
+        return detail;
+    }
+
+    public void setDetail(byte[] detail) {
+        this.detail = detail;
+    }
+}

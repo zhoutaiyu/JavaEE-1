8af503e24a8783611e8c25e24f2154f2a2ab9a36
李欣 <1191863273@qq.com>
2016-11-20 14:47:05 +0800

    update

---
+++ SimpleSaleSystem/src/main/java/cn/qdgxy/sss/controller/interceptor/AdminInterceptor.java
@@ -0,0 +1,38 @@
+package cn.qdgxy.sss.controller.interceptor;
+
+import cn.qdgxy.sss.po.User;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 管理员登录拦截器
+ * create:2016-11-20 14:18
+ *
+ * @author 李欣
+ * @version 1.0
+ */
+public class AdminInterceptor implements HandlerInterceptor {
+
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        User user = (User) request.getSession().getAttribute("user");
+        if (user != null && user.getUsertype() == 1) {
+            return true;
+        }
+
+        response.sendRedirect("login");
+        return false;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+    }
+}

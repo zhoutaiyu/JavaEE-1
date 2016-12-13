3873a49288ee61e2c72c42dc09783bcdd12d157a
李欣 <1191863273@qq.com>
2016-12-13 21:53:32 +0800

    init

---
+++ SSMP/src/main/java/cn/qdgxy/ssm/mapper/OrderDetailMapper.java
@@ -0,0 +1,32 @@
+package cn.qdgxy.ssm.mapper;
+
+import cn.qdgxy.ssm.po.OrderDetail;
+import cn.qdgxy.ssm.po.OrderDetailExample;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface OrderDetailMapper {
+
+    long countByExample(OrderDetailExample example);
+
+    int deleteByExample(OrderDetailExample example);
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(OrderDetail record);
+
+    int insertSelective(OrderDetail record);
+
+    List<OrderDetail> selectByExample(OrderDetailExample example);
+
+    OrderDetail selectByPrimaryKey(Integer id);
+
+    int updateByExampleSelective(@Param("record") OrderDetail record, @Param("example") OrderDetailExample example);
+
+    int updateByExample(@Param("record") OrderDetail record, @Param("example") OrderDetailExample example);
+
+    int updateByPrimaryKeySelective(OrderDetail record);
+
+    int updateByPrimaryKey(OrderDetail record);
+}

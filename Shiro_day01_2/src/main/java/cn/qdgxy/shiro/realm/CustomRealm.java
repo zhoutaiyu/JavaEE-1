178e5486dad471370a2a6d87408a9528d9923ddd
李欣 <1191863273@qq.com>
2016-12-23 21:34:57 +0800

    finish

--- Shiro_day01_2/src/main/java/cn/qdgxy/shiro/realm/CustomRealm.java
+++ Shiro_day01_2/src/main/java/cn/qdgxy/shiro/realm/CustomRealm.java
@@ -59,7 +59,6 @@ public class CustomRealm extends AuthorizingRealm {
     @Override
     protected AuthorizationInfo doGetAuthorizationInfo(
             PrincipalCollection principals) {
-
         //从 principals获取主身份信息
         //将getPrimaryPrincipal方法返回值转为真实身份类型（在上边的doGetAuthenticationInfo认证通过填充到SimpleAuthenticationInfo中身份类型），
         String userCode = (String) principals.getPrimaryPrincipal();
@@ -67,12 +66,12 @@ public class CustomRealm extends AuthorizingRealm {
         //根据身份信息获取权限信息
         //连接数据库...
         //模拟从数据库获取到数据
-        List<String> permissions = new ArrayList<String>();
+        List<String> permissions = new ArrayList<>();
         permissions.add("user:create");//用户的创建
         permissions.add("items:add");//商品添加权限
         //....
 
-        //查到权限数据，返回授权信息(要包括 上边的permissions)
+        //查到权限数据，返回授权信息(要包括上边的permissions)
         SimpleAuthorizationInfo simpleAuthorizationInfo = new SimpleAuthorizationInfo();
         //将上边查询到授权信息填充到simpleAuthorizationInfo对象中
         simpleAuthorizationInfo.addStringPermissions(permissions);

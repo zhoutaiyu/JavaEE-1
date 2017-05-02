8cab1ca1d1c79a6add4989d10ee6d65bad0f7d53
李欣 <li1191863273@hotmail.com>
2017-05-02 15:36:57 +0800

    update

--- MybatisPlusTool/src/main/java/cn/qdgxy/util/MpGenerator.java
+++ MybatisPlusTool/src/main/java/cn/qdgxy/util/MpGenerator.java
@@ -1,27 +1,23 @@
 package cn.qdgxy.util;
 
 import com.baomidou.mybatisplus.generator.AutoGenerator;
-import com.baomidou.mybatisplus.generator.InjectionConfig;
 import com.baomidou.mybatisplus.generator.config.DataSourceConfig;
 import com.baomidou.mybatisplus.generator.config.GlobalConfig;
 import com.baomidou.mybatisplus.generator.config.PackageConfig;
 import com.baomidou.mybatisplus.generator.config.StrategyConfig;
+import com.baomidou.mybatisplus.generator.config.converts.MySqlTypeConvert;
+import com.baomidou.mybatisplus.generator.config.rules.DbColumnType;
 import com.baomidou.mybatisplus.generator.config.rules.DbType;
 import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
 
-import java.util.HashMap;
-import java.util.Map;
-
 /**
- * MybatisPlus代码生成
- * create:2016-12-13 20:48
- *
- * @author 李欣
- * @version 1.0
+ * <p>
+ * 代码生成器演示
+ * </p>
  */
-public class MpGeneratorCustom {
+public class MpGenerator {
 
-    private static final String PARENT_NAME = "cn.qdgxy";    // 包名
+    private static final String PACKAGE_NAME = "cn.qdgxy";    // 包名
     private static final String MODULE_NAME = "tr";    // 包名
     private static final String SAVE_DIR = "D://";  //存放路径
     private static final String AUTHOR = "liXin";  //作者
@@ -30,7 +26,7 @@ public class MpGeneratorCustom {
     private static final String DRIVER_NAME = "com.mysql.jdbc.Driver";
     private static final String USERNAME = "root";
     private static final String PASSWORD = "1234";
-    private static final String URL = "jdbc:mysql://127.0.0.1/qdgxytr" +
+    private static final String URL = "jdbc:mysql://127.0.0.1:3306/qdgxytr" +
             "?useUnicode=true&characterEncoding=UTF8&serverTimezone=UTC&useSSL=false";
 
     /**
@@ -40,6 +36,7 @@ public class MpGeneratorCustom {
      */
     public static void main(String[] args) {
         AutoGenerator mpg = new AutoGenerator();
+
         // 全局配置
         GlobalConfig gc = new GlobalConfig();
         gc.setOutputDir(SAVE_DIR);
@@ -53,7 +50,7 @@ public class MpGeneratorCustom {
         // 自定义文件命名，注意 %s 会自动填充表实体属性！
         gc.setMapperName("%sMapper");
         gc.setXmlName("%sMapper");
-        gc.setServiceName("%sService");
+        gc.setServiceName("MP%sService");
         gc.setServiceImplName("%sServiceImpl");
         gc.setControllerName("%sController");
         mpg.setGlobalConfig(gc);
@@ -61,6 +58,14 @@ public class MpGeneratorCustom {
         // 数据源配置
         DataSourceConfig dsc = new DataSourceConfig();
         dsc.setDbType(DbType.MYSQL);
+        dsc.setTypeConvert(new MySqlTypeConvert() {
+            // 自定义数据库表字段类型转换【可选】
+            @Override
+            public DbColumnType processTypeConvert(String fieldType) {
+                System.out.println("转换类型：" + fieldType);
+                return super.processTypeConvert(fieldType);
+            }
+        });
         dsc.setDriverName(DRIVER_NAME);
         dsc.setUsername(USERNAME);
         dsc.setPassword(PASSWORD);
@@ -69,13 +74,11 @@ public class MpGeneratorCustom {
 
         // 策略配置
         StrategyConfig strategy = new StrategyConfig();
-        //strategy.setTablePrefix("bmd_");// 此处可以修改为您的表前缀
+        // strategy.setCapitalMode(true);// 全局大写命名 ORACLE 注意
+        // strategy.setTablePrefix(new String[]{"tlog_", "tsys_"});// 此处可以修改为您的表前缀
         strategy.setNaming(NamingStrategy.underline_to_camel);// 表名生成策略
-        //strategy.setInclude(new String[] { "user" }); // 需要生成的表
+        // strategy.setInclude(new String[] { "user" }); // 需要生成的表
         // strategy.setExclude(new String[]{"test"}); // 排除生成的表
-
-        // 字段名生成策略
-        strategy.setFieldNaming(NamingStrategy.underline_to_camel);
         // 自定义实体父类
         // strategy.setSuperEntityClass("com.baomidou.demo.TestEntity");
         // 自定义实体，公共字段
@@ -98,25 +101,52 @@ public class MpGeneratorCustom {
 
         // 包配置
         PackageConfig pc = new PackageConfig();
-        pc.setParent(PARENT_NAME);
+        pc.setParent(PACKAGE_NAME);
         pc.setModuleName(MODULE_NAME);
         pc.setEntity("po");
         pc.setXml("mapper");
         pc.setController("web.controller");
         mpg.setPackageInfo(pc);
 
-        // 注入自定义配置，可以在 VM 中使用 cfg.abc 设置的值
-        InjectionConfig cfg = new InjectionConfig() {
-            @Override
-            public void initMap() {
-                Map<String, Object> map = new HashMap<String, Object>();
-                map.put("abc", this.getConfig().getGlobalConfig().getAuthor() + "-mp");
-                this.setMap(map);
-            }
-        };
-        mpg.setCfg(cfg);
+        // 注入自定义配置，可以在 VM 中使用 cfg.abc 【可无】
+        //InjectionConfig cfg = new InjectionConfig() {
+        //    @Override
+        //    public void initMap() {
+        //        Map<String, Object> map = new HashMap<String, Object>();
+        //        map.put("abc", this.getConfig().getGlobalConfig().getAuthor() + "-mp");
+        //        this.setMap(map);
+        //    }
+        //};
 
-        // 自定义模板配置
+        // 自定义 xxList.jsp 生成
+        //List<FileOutConfig> focList = new ArrayList<FileOutConfig>();
+        //focList.add(new FileOutConfig("/template/list.jsp.vm") {
+        //    @Override
+        //    public String outputFile(TableInfo tableInfo) {
+        //        // 自定义输入文件名称
+        //        return "D://my_" + tableInfo.getEntityName() + ".jsp";
+        //    }
+        //});
+        //cfg.setFileOutConfigList(focList);
+        //mpg.setCfg(cfg);
+
+        // 调整 xml 生成目录演示
+        //focList.add(new FileOutConfig("/templates/mapper.xml.vm") {
+        //    @Override
+        //    public String outputFile(TableInfo tableInfo) {
+        //        return "/develop/code/xml/" + tableInfo.getEntityName() + ".xml";
+        //    }
+        //});
+        //cfg.setFileOutConfigList(focList);
+        //mpg.setCfg(cfg);
+
+        // 关闭默认 xml 生成，调整生成 至 根目录
+        //TemplateConfig tc = new TemplateConfig();
+        //tc.setXml(null);
+        //mpg.setTemplate(tc);
+
+        // 自定义模板配置，可以 copy 源码 mybatis-plus/src/main/resources/template 下面内容修改，
+        // 放置自己项目的 src/main/resources/template 目录下, 默认名称一下可以不配置，也可以自定义模板名称
         // TemplateConfig tc = new TemplateConfig();
         // tc.setController("...");
         // tc.setEntity("...");
@@ -124,12 +154,11 @@ public class MpGeneratorCustom {
         // tc.setXml("...");
         // tc.setService("...");
         // tc.setServiceImpl("...");
+        // 如上任何一个模块如果设置 空 OR Null 将不生成该模块。
         // mpg.setTemplate(tc);
 
         // 执行生成
         mpg.execute();
-        // 打印注入设置
-        System.err.println(mpg.getCfg().getMap().get("abc"));
     }
 
 }
